b746b0ab754081e7d89e5cca1254cf097545ba67
rfbatista <rfbatista@gmail.com>
2024-05-06 11:20:34 -0300

    feat: increse timeout process file

--- internal/infra/photoshop.go
+++ internal/infra/photoshop.go
@@ -43,7 +43,7 @@ func (p PhotoshopProcessor) ProcessFile(
 	}
 	req.Header.Set("Content-Type", "application/json")
 	client := http.Client{
-		Timeout: 30 * time.Second,
+		Timeout: 90 * time.Second,
 	}
 	res, err := client.Do(req)
 	p.log.Info("file processing finished")
