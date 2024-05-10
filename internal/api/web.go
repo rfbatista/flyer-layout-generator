cd8d07b8a3b72f28e75042b9adb50609601f4060
rfbatista <rfbatista@gmail.com>
2024-05-09 21:23:55 -0300

    feat: web api

---
+++ internal/api/web.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/labstack/echo/v4"
+	"github.com/rfbatista/apitools"
+	"go.uber.org/zap"
+
+	"algvisual/internal/database"
+	"algvisual/web/views/createtemplatepage"
+)
+
+func NewPageCreateTemplate(
+	queries *database.Queries,
+	conn *pgxpool.Pool,
+	log *zap.Logger,
+) apitools.Handler {
+	h := apitools.NewHandler()
+	h.SetMethod(apitools.GET)
+	h.SetPath("/criar")
+	h.SetHandle(func(c echo.Context) error {
+		component := createtemplatepage.CreateTemplatePage()
+		w := c.Response().Writer
+		err := component.Render(c.Request().Context(), w)
+		if err != nil {
+			log.Error("failed to render home page", zap.Error(err))
+			return err
+		}
+		return nil
+	})
+	return h
+}
