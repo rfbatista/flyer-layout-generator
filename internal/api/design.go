c61ce5a0aad0518fb29656cb7aacb4ede33a2f33
rfbatista <rfbatista@gmail.com>
2024-05-09 21:23:49 -0300

    refactor: renaming api

--- internal/api/design.go
+++ internal/api/design.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -30,6 +31,130 @@ func NewGenerateDesignAPI(
 			return c.String(http.StatusBadRequest, "bad request")
 		}
 		result, err := usecases.GenerateDesignUseCase(c.Request().Context(), req, client, queries)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, result)
+	})
+	return h
+}
+
+func NewUploadDesignAPI(
+	db *database.Queries,
+	proc *infra.PhotoshopProcessor,
+	storage infra.FileStorage,
+	log *zap.Logger,
+) apitools.Handler {
+	h := apitools.NewHandler()
+	h.SetMethod(apitools.POST)
+	h.SetPath(shared.EndpointUploadPhotoshop.String())
+	h.SetHandle(func(c echo.Context) error {
+		file, err := c.FormFile("file")
+		if err != nil {
+			return err
+		}
+		src, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+		req := usecases.UploadPhotoshopFileUseCaseRequest{
+			Filename: c.FormValue("filename"),
+			File:     src,
+		}
+		out, err := usecases.UploadPhotoshopFileUseCase(
+			c.Request().Context(),
+			db,
+			req,
+			storage.Upload,
+			proc.ProcessFile,
+			log,
+		)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, out)
+	})
+	return h
+}
+
+func NewListDesignElementsAPI(db *database.Queries) apitools.Handler {
+	h := apitools.NewHandler()
+	h.SetMethod(apitools.GET)
+	h.SetPath(shared.EndpointListPhotoshopElements.String())
+	h.SetHandle(func(c echo.Context) error {
+		fmt.Println("teste")
+		var req usecases.ListDesignElementsUseCaseRequest
+		err := c.Bind(&req)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		result, err := usecases.ListDesignElementsUseCase(c.Request().Context(), req, db)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, result)
+	})
+	return h
+}
+
+func NewListDesignFilesAPI(db *database.Queries, log *zap.Logger) apitools.Handler {
+	h := apitools.NewHandler()
+	h.SetMethod(apitools.GET)
+	h.SetPath(shared.EndpointListPhotoshop.String())
+	h.SetHandle(func(c echo.Context) error {
+		var req usecases.ListPhotoshopFilesRequest
+		err := c.Bind(&req)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		result, err := usecases.ListPhotoshopFilesUseCase(c.Request().Context(), req, db, log)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, result)
+	})
+	return h
+}
+
+func NewCreateComponentAPI(
+	db *database.Queries,
+	log *zap.Logger,
+	conn *pgxpool.Pool,
+) apitools.Handler {
+	h := apitools.NewHandler()
+	h.SetMethod(apitools.POST)
+	h.SetPath(shared.EndpointCreateComponent.String())
+	h.SetHandle(func(c echo.Context) error {
+		var req usecases.CreateComponentRequest
+		err := c.Bind(&req)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		result, err := usecases.CreateComponentUseCase(c.Request().Context(), req, db, conn, log)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, result)
+	})
+	return h
+}
+
+func NewGetDesignByIDAPI(
+	db *database.Queries,
+	log *zap.Logger,
+	conn *pgxpool.Pool,
+) apitools.Handler {
+	h := apitools.NewHandler()
+	h.SetMethod(apitools.GET)
+	h.SetPath(shared.EndpointCreateComponent.String())
+	h.SetHandle(func(c echo.Context) error {
+		var req usecases.GetDesignByIdRequest
+		err := c.Bind(&req)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		result, err := usecases.GetPhotoshopByIdUseCase(c.Request().Context(), req, db, log)
 		if err != nil {
 			return err
 		}
