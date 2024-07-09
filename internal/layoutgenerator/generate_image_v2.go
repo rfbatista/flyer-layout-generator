3888ddab643ea6dd1a04da3f245799a63e6a1115
Renan Batista <renan.batista@gruponewway.com.br>
2024-07-09 03:12:34 -0300

    add 25 padding

--- internal/layoutgenerator/generate_image_v2.go
+++ internal/layoutgenerator/generate_image_v2.go
@@ -56,7 +56,7 @@ func GenerateImageV2UseCase(
 		TemplateID: req.TemplateID,
 		LayoutID:   req.LayoutID,
 		DesignID:   out.Layout.DesignID,
-		Padding:    req.Padding,
+		Padding:    25,
 		Priorities: req.Priorities,
 	}, db, log, out.Layout.Width, out.Layout.Height)
 	if err != nil {
