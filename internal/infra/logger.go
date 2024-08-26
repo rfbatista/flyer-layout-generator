1b44f2882e85f1b73791de8fc11a9a7519010012
Renan Batista <renan.batista@gruponewway.com.br>
2024-08-26 13:50:38 -0300

    refactor: remove file logger

--- internal/infra/logger.go
+++ internal/infra/logger.go
@@ -2,7 +2,6 @@ package infra
 
 import (
 	"algvisual/internal/infra/config"
-	"path/filepath"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -13,14 +12,14 @@ func NewFileLogger() {}
 func NewLogger(cfg *config.AppConfig) *zap.Logger {
 	config := zap.NewDevelopmentConfig()
 	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	if cfg.IsProd() {
-		config = zap.NewProductionConfig()
-		fullpath := filepath.Join(cfg.LogPath, "proxy.log")
-		config.Level = zap.NewAtomicLevel()
-		config.OutputPaths = []string{
-			fullpath,
-		}
-	}
+	// if cfg.IsProd() {
+	// 	config = zap.NewProductionConfig()
+	// 	fullpath := filepath.Join(cfg.LogPath, "proxy.log")
+	// 	config.Level = zap.NewAtomicLevel()
+	// 	config.OutputPaths = []string{
+	// 		fullpath,
+	// 	}
+	// }
 	logger, _ := config.Build()
 	return logger
 }
